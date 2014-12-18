03b1f6bad6add731508e72c62c3ce655a60d2d4b
kerim1 <kerim.meijer@gmail.com>
2014-12-18 10:23:21 +0100

    Migrated the configuration and most of the import procedures

---
+++ themes/iish/theme.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'extends' => 'blueprint',
+    'css' => array(),
+    'js' => array(),
+    'favicon' => 'favicon.ico',
+    'helpers' => array(
+        'factories' => array(),
+        'invokables' => array()
+    )
+);
