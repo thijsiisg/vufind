a76abf875188cb88ea3f6825fea9172a0dea70ef
Lucien van Wouw <lwo@iisg.nl>
2015-02-09 16:12:42 +0100

    Add build script

---
+++ make.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# make.sh
+#
+# Build a package
+
+set -e
+
+instance=$1
+if [ -z "$instance" ] ; then
+    instance="vufind"
+	echo "Default version ${vufind}"
+fi
+
+version=$2
+if [ -z "$version" ] ; then
+	version="2.3.1"
+	echo "Default version ${version}"
+fi
+
+if [ -d target ] ; then
+    rm -rf target
+fi
+mkdir target
+
+
+revision=$(git rev-parse HEAD)
+app=$instance-$version
+expect=target/$app.tar.gz
+
+echo "Build $expect, revision $revision"
+
+app=$instance-$version
+if [ -d $app ] ; then
+    rm -rf $app
+fi
+
+
+rsync -av --exclude='.git' --exclude='.gitignore' --exclude='make.sh' . $app
+# set permissions
+for f in $(find "$app" -type f  -name "*.sh" )
+do
+    chmod 744 $f
+done
+
+cd $app
+composer update
+
+echo $revision>$app/revision.txt
+tar -zcvf $expect $app
+rm -rf $app
+
+if [ -f $expect ] ; then
+    echo "Build ok."
+else
+	echo -e "Unable to build ${expect}"
+fi
