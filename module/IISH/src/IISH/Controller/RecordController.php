1d2e4ed51a43bb01ea813875411c371aa2d5d0e3
Lucien van Wouw <lwo@iisg.nl>
2015-03-15 00:43:23 +0100

    Add File Controller

--- module/IISH/src/IISH/Controller/RecordController.php
+++ module/IISH/src/IISH/Controller/RecordController.php
@@ -98,7 +98,7 @@ class RecordController extends VuFindRecordController {
     private function exportToPDF() {
         $pdfLink = isset($this->iishConfig->PDF->link) ? $this->iishConfig->PDF->link : 'PDF';
         $driver = $this->loadRecord();
-        $url = $this->url()->fromRoute('home') . $pdfLink . '?id=' . $driver->getUniqueID() . '&contentType=application/pdf';
+        $url = $this->url()->fromRoute('home') . $pdfLink . '?id=' . $driver->getUniqueID() . '.pdf&contentType=application/pdf';
         return $this->redirect()->toUrl($url);
     }
 } 
