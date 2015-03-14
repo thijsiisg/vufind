640b4b12b041939067f5862e717b8789e638d379
Lucien van Wouw <lwo@iisg.nl>
2015-03-14 18:44:29 +0100

    Add File Controller

--- local/import/fop-iish.archieven.sh
+++ local/import/fop-iish.archieven.sh
@@ -6,9 +6,10 @@
 
 
 pdf_folder="/usr/local/vufind/local/cache/pdf"
-for file in ${pdf_folder}/*.xml
+cd $pdf_folder
+for file in *.xml
 do
-    pdf="{$pdf_folder}/$(basename $file .xml).pdf"
+    pdf="{$pdf_folder}/$(basename $file).pdf"
     echo "Creating $pdf from $file"
     fop -c /usr/local/vufind/local/import/fop/fop.xconf -xml $file -xsl /usr/local/vufind/local/import/xsl/ead_complete_fo.xsl -pdf $pdf -param path /usr/local/vufind/local/import/xsl -param sysYear $(date +'%Y')
     if [ -f "$pdf" ] ; then
