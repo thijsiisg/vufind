d28f69966aceccc716d0531bc9d9ca875adfa221
Lucien van Wouw <lwo@iisg.nl>
2015-03-15 11:38:58 +0100

    Remove fop.jar as it is installed as a package. Rearrange xsl into the fop folder.

--- local/import/fop/iish.archieven.sh
+++ local/import/fop/iish.archieven.sh
@@ -6,14 +6,15 @@
 
 
 pdf_folder="/usr/local/vufind/local/cache/pdf"
-for file in "${pdf_folder}/*.xml"
+for file in "$pdf_folder"/*.xml
 do
-    pdf="${pdf_folder}/$(basename $file).pdf"
+    filename=$(basename $file)
+    pdf="${pdf_folder}/${filename:0:9}.pdf"
     echo "Creating $pdf from $file"
-    /usr/bin/fop -c /usr/local/vufind/local/import/fop/fop.xconf -xml $file -xsl /usr/local/vufind/local/import/fop/xsl/iish.archieven/ead_complete_fo.xsl -pdf $pdf -param path /usr/local/vufind/local/import/fop/xsl -param sysYear $(date +'%Y')
+    /usr/bin/fop -c /usr/local/vufind/local/import/fop/fop.xconf -xml $file -xsl /usr/local/vufind/local/import/fop/xsl/iish.archieven/ead_complete_fo.xsl -pdf $pdf -param path /usr/local/vufind/local/import/fop/xsl/iish.archieven -param sysYear $(date +'%Y')
     if [ -f "$pdf" ] ; then
-	    chown www-data:www-data "$pdf"
-	    chmod 600 "$pdf"
+        chown www-data:www-data "$pdf"
+        chmod 400 "$pdf"
+        rm $file
     fi
-    rm $file
 done
