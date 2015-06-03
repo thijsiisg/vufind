2d506080b13544b84ba028b13aba3d34c2898e6f
kerim1 <kerim.meijer@gmail.com>
2015-06-03 11:34:19 +0200

    Improved homepage facet 'browse archives by region'

--- themes/iish/js/home.js
+++ themes/iish/js/home.js
@@ -16,4 +16,20 @@ $(document).ready(function () {
             to: 0.05
         }
     });
+
+    $('.toggle-more').click(function (e) {
+        var parent = $(this).closest('.facetList');
+        parent.find('.show-minimal').addClass('hidden');
+        parent.find('.show-all').removeClass('hidden');
+
+        e.preventDefault();
+    });
+
+    $('.toggle-less').click(function () {
+        var parent = $(this).closest('.facetList');
+        parent.find('.show-all').addClass('hidden');
+        parent.find('.show-minimal').removeClass('hidden');
+
+        e.preventDefault();
+    });
 });
