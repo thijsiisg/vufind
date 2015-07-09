bbac05398eb974f1d43fb3af6c9f090ed9a621ac
Lucien van Wouw <lwo@iisg.nl>
2015-07-09 23:15:24 +0200

    Disable sslverifypeer

--- local/rm_search_cache_3005100_images.sh
+++ local/rm_search_cache_3005100_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find $VUFIND_SHARE/cache/large    -name '*.jpg' -delete
-find $VUFIND_SHARE/cache/medium   -name '*.jpg' -delete
-find $VUFIND_SHARE/cache/small    -name '*.jpg' -delete
-rm $VUFIND_SHARE/cache/xml/*
+find $VUFIND_CACHE_CACHE_DIR/cache/large    -name '*.jpg' -delete
+find $VUFIND_CACHE_CACHE_DIR/cache/medium   -name '*.jpg' -delete
+find $VUFIND_CACHE_CACHE_DIR/cache/small    -name '*.jpg' -delete
+rm $VUFIND_CACHE_CACHE_DIR/cache/xml/*
