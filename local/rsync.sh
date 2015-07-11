cad6ed4c78752f45934eb1130e23ab04c6732e46
Lucien van Wouw <lwo@iisg.nl>
2015-07-11 16:37:03 +0200

    Sync

--- local/rsync.sh
+++ local/rsync.sh
@@ -2,9 +2,13 @@
 #
 # Syncs the PDF documents and authority databases.
 
+datestamp=$(date +"%Y-%m-%d")
+log="/data/log/rsync-${datestamp}.log"
+
 for slave in $VUFIND_SLAVES
 do
     host=$slave.iisg.net
-    sudo -u vufind rsync -av --progress "${VUFIND_SHARE}/solr/${HOSTNAME}/alphabetical_browse" "${host}:/${VUFIND_SHARE}/solr/${slave}/"
-    sudo -u vufind rsync -av --progress "${VUFIND_CACHE_CACHE_DIR}/pdf/"*.pdf "${host}:/${VUFIND_CACHE_CACHE_DIR}/pdf"
+    echo "Rsync to ${host}" >> $log
+    sudo -u vufind rsync -av --progress "${VUFIND_SHARE}/solr/${HOSTNAME}/alphabetical_browse" "${host}:/${VUFIND_SHARE}/solr/${slave}/" >> $log
+    sudo -u vufind rsync -av --progress "${VUFIND_CACHE_CACHE_DIR}/pdf/"*.pdf "${host}:/${VUFIND_CACHE_CACHE_DIR}/pdf" >> $log
 done
