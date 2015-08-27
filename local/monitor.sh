c51c6128251ec0b578e3ceb5e33531551c8a7b5a
Gordan Cupac (IISG) <gordan-cupac@users.noreply.github.com>
2015-08-27 09:44:41 +0200

    increased timeouts

--- local/monitor.sh
+++ local/monitor.sh
@@ -23,22 +23,22 @@ fi
 s=/opt/status.txt
 q="http://localhost:8080/solr/biblio/select"
 O=/tmp/status.txt
-wget --spider -T 3 -t 3 -O $O $q
+wget --spider -T 5 -t 3 -O $O $q
 rc=$?
 if [[ $rc == 0 ]] ; then
     echo "$(date)">$f
     exit 0
 else
     rm -f $f
-	echo "$(date): Invalid response ${rc}" >> $s
-	service vufind stop
-    sleep 5
-	killall java
-	sleep 5
-	service vufind start
-    sleep 20
+    echo "$(date): Invalid response ${rc}" >> $s
+    service vufind stop
+    sleep 7
+    killall java
+    sleep 7
+    service vufind start
+    sleep 30
 
-	subject="Automatic restart by ${0}"
+    subject="Automatic restart by ${0}"
     /usr/bin/sendmail --body "$s" --from "search@${HOSTNAME}" --to "$VUFIND_SITE_EMAIL" --subject "$subject" --mail_relay "$VUFIND_MAIL_HOST"
 
     exit 1
