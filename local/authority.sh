368d36d42f99c36a77c6565a320a7518b5cc68e5
Lucien van Wouw <lwo@iisg.nl>
2015-09-25 17:34:34 +0200

    SEARCHM-67

--- local/authority.sh
+++ local/authority.sh
@@ -20,8 +20,8 @@ if [ -d "/data/solr/${HOSTNAME}/authority/index" ] ; then
 else
     rm -rf "/data/solr/${HOSTNAME}/authority/"*
     service vufind restart
-    body=/tmp/a
-    subject="${HOSTNAME} - Authority index gone. Automatic restart by ${0}"
+    body=/dev/null
+    subject="${HOSTNAME} - Could not find an authority index. Automatic restart vufind by ${HOSTNAME} was required"
     /usr/bin/sendmail --body "$body" --from "search@${HOSTNAME}" --to "$MAIL_TO" --subject "$subject" --mail_relay "$VUFIND_MAIL_HOST"
     exit 1
 fi
