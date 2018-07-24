e6b68081bb7f68b75173df4971f09aeb5d06d024
Lucien van Wouw <lwo@iisg.nl>
2018-07-24 21:40:12 +0200

    Use https protocol

--- local/rsync.sh
+++ local/rsync.sh
@@ -7,7 +7,7 @@ log="/data/log/rsync-${datestamp}.log"
 
 for slave in $VUFIND_SLAVES
 do
-    host=$slave.iisg.net
+    host=$slave
     echo "Rsync to ${host}" >> $log
     sudo -u vufind rsync -av --progress "${VUFIND_SHARE}/solr/${HOSTNAME}/alphabetical_browse" "${host}:/${VUFIND_SHARE}/solr/${slave}/" >> $log
     sudo -u vufind rsync -av --progress --include '*.pdf' "${VUFIND_CACHE_CACHE_DIR}/pdf/" "${host}:/${VUFIND_CACHE_CACHE_DIR}/pdf" >> $log
